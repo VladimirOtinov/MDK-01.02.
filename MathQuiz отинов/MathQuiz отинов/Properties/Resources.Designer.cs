9dc5002d6d942af7b4e9c6372cc5ffcec3c49722
Vladimir <118179352+VladimirOtinov@users.noreply.github.com>
2023-01-19 16:15:55 +0300

    Update Resources.Designer.cs

--- MathQuiz отинов/MathQuiz отинов/Properties/Resources.Designer.cs
+++ MathQuiz отинов/MathQuiz отинов/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace MathQuiz_Сивунов.Properties
+namespace MathQuiz_отинов.Properties
 {
 
 
